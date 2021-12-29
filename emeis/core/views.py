190c2a6d2ca05a7f862d84623d4675a730b03ad4
Wiktork <wiktor.kepczynski@adfinis.com>
2021-12-29 17:35:38 +0100

    feat: add user list export as xlsx file

--- emeis/core/views.py
+++ emeis/core/views.py
@@ -1,7 +1,14 @@
+from tempfile import NamedTemporaryFile
+
+import openpyxl
+from django.conf import settings
 from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.utils.translation import gettext as _
 from generic_permissions.permissions import PermissionViewMixin
 from generic_permissions.visibilities import VisibilityViewMixin
+from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
@@ -56,6 +63,45 @@ class UserViewSet(BaseViewset):
         "=zip",
         "city",
     )
+
+    @action(methods=["get"], detail=False)
+    def export(self, request):
+        """Export user list as excel table."""
+        queryset = self.get_queryset()
+
+        # we use a template to set the headers to bold and align the columns to fit on a DIN-A4 page
+        workbook = openpyxl.load_workbook(settings.USER_EXPORT_TEMPLATE_PATH)
+        sheet = workbook.active
+
+        # set column headers
+        sheet["A1"] = _("Name")
+        sheet["B1"] = _("First Name")
+        sheet["C1"] = _("Email")
+        sheet["D1"] = _("Roles and organizations")
+
+        for row, user in enumerate(queryset.iterator(), start=2):
+            acl_string = "\n".join([
+                f"{acl.role.name}: {acl.scope.full_name()}"
+                for acl in user.acls.all().select_related("scope", "role").order_by("role__name")
+            ])
+
+            sheet[f"A{row}"] = user.last_name
+            sheet[f"B{row}"] = user.first_name
+            sheet[f"C{row}"] = user.email
+            sheet[f"D{row}"] = acl_string
+            sheet[f"D{row}"].alignment = openpyxl.styles.Alignment(wrap_text=True)
+
+        # save workbook as temporary file and read the file
+        with NamedTemporaryFile() as tmp:
+            workbook.save(tmp.name)
+            tmp.seek(0)
+            stream = tmp.read()
+            workbook.close()
+            tmp.close()
+
+        response = HttpResponse(stream, content_type="application/vnd.ms-excel")
+        response["Content-Disposition"] = "attachment; filename=export.xlsx"
+        return response
 
 
 class ScopeViewSet(BaseViewset):
